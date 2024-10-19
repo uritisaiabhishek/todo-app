0ad7321dfd6f63d939af515e69128f793bbb0a70
uritisaiabhishek <uritisaiabhishek@gmail.com>
2024-10-20 01:04:44 +0530

    console cleanup

--- src/firebase.js
+++ src/firebase.js
@@ -13,8 +13,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
 };
 
-console.log('Firebase Config:', firebaseConfig);
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
