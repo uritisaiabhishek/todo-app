c9e59f18bf08a0d3ac8aaf80bf1f23dc3c20ec7e
uritisaiabhishek <uritisaiabhishek@gmail.com>
2024-10-20 00:43:37 +0530

    test firebase checks

--- src/firebase.js
+++ src/firebase.js
@@ -13,14 +13,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
 };
 
-console.log('Firebase Config:', {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-});
+console.log('Firebase Config:', firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
